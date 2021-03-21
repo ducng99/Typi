244dea849f7a2ad3e05ea107c5a0821b373b748b
ducng99 <tuanduc249.td@gmail.com>
2021-03-21 19:59:20 +1300

    Small changes to frontend

--- frontend/src/components/ChatComponents/ReceiverPanel.vue
+++ frontend/src/components/ChatComponents/ReceiverPanel.vue
@@ -1,8 +1,18 @@
 <template>
-<div class="p-3">
+<div class="py-3">
     <div class="d-flex flex-column align-items-center">
         <b-avatar size="5rem"></b-avatar>
         <div class="my-2"><b>{{ receiver.Username }}</b></div>
+    </div>
+    <div class="mt-3">
+        <div :class="'d-flex p-3 rounded align-items-center ' + $style.menu_entry">
+            <b-icon icon="person-dash-fill" class="mr-3"/>
+            Unfriend
+        </div>
+        <div :class="'d-flex p-3 rounded align-items-center ' + $style.menu_entry">
+            <b-icon icon="dash-circle-fill" class="mr-3"/>
+            Block
+        </div>
     </div>
 </div>
 </template>
