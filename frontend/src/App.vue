6b871613f0d1d0b94639d1a56369c0b05c9831ce
Duc Nguyen <tuanduc249.td@gmail.com>
2021-03-24 22:48:35 +1300

    Fixed wrong SecureStorage path

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -91,7 +91,7 @@ export default {
                 else
                 {
                     this.$cookies.remove(this.$COOKIE_SESSION_ID);
-                    import('../SecureStorage').then(SecureStorage => {
+                    import('./SecureStorage').then(SecureStorage => {
                         SecureStorage.passwordHash = '';
                     });
                 }
